5c08072d5c0fdd20789b4e87a64e98c114f2c29d
Mbali-Nyalungu <Mbali.Nyalungu04@gmail.com>
2024-09-03 11:27:12 +0200

    added home component route

--- school_app/src/app/app.routes.ts
+++ school_app/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+    {
+        path: "",
+        title: "home",
+        component: HomeComponent,
+
+    },
+];
