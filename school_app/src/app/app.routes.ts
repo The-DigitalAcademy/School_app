23223e2be0dd39bdde1fc3ce3957509af6017ac5
khosatho <monokhosatho@gmail.com>
2024-09-05 16:26:17 +0200

    changed components,placed pictures

--- school_app/src/app/app.routes.ts
+++ school_app/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ActivitesComponent } from './pages/activites/activites.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path:'activities', component: ActivitesComponent
+}];
