9f580d2f2b428c5ac3fc58187f2a52c3d7177a4c
Mbali-Nyalungu <Mbali.Nyalungu04@gmail.com>
2024-09-09 09:53:21 +0200

    git removed routes because of conflict

--- school_app/src/app/app.routes.ts
+++ school_app/src/app/app.routes.ts
@@ -1,16 +1,11 @@
 import { Routes } from '@angular/router';
 
-import { ArchiversComponent } from './pages/archivers/archivers.component';
 
 export const routes: Routes = [
 
    
 
-    {    path:"archivers",
-         title: "achievers",
-        component: ArchiversComponent,
-
-    },
+   
 
 
     
