b75d3ef3b3d5015bb1e9cb0f2e6561f73ce635c9
reeri15 <reshay.makunyane@gmail.com>
2024-09-09 08:47:59 +0200

    Error page completed

--- school_app/src/app/app.routes.ts
+++ school_app/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ErrorComponent } from './pages/error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+path: '**',
+component: ErrorComponent,
+
+    },
+];
