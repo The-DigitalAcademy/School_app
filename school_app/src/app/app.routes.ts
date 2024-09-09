2c84fa2e595ae723812e1a14cc1e1c9cf74aa5d9
Mbali-Nyalungu <Mbali.Nyalungu04@gmail.com>
2024-09-09 09:17:57 +0200

    fixed routes

---
+++ school_app/src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+
+import { ArchiversComponent } from './pages/archivers/archivers.component';
+
+export const routes: Routes = [
+
+   
+
+    {    path:"archivers",
+         title: "achievers",
+        component: ArchiversComponent,
+
+    },
+
+
+    
+
+];
