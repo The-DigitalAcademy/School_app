7e1e8660a2e498bbe09fbc55e2f967b05ca11d51
Nathandiv <k.n.hlungwani@gmail.com>
2024-09-11 15:32:18 +0200

    added footer bon activities

--- school_app/src/app/pages/activites/activites.component.ts
+++ school_app/src/app/pages/activites/activites.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../../UI/shared/nav/nav.component';
+import { FooterComponent } from '../../UI/shared/footer/footer.component';
 
 @Component({
   selector: 'app-activites',
   standalone: true,
-  imports: [NavComponent],
+  imports: [NavComponent,FooterComponent],
   templateUrl: './activites.component.html',
   styleUrl: './activites.component.css'
 })
