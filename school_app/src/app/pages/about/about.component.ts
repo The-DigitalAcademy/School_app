d1527c56841f193e86ae4183c46b69683ef4d574
Nathandiv <k.n.hlungwani@gmail.com>
2024-09-11 15:31:00 +0200

    added the footer on the about

--- school_app/src/app/pages/about/about.component.ts
+++ school_app/src/app/pages/about/about.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../../UI/shared/nav/nav.component';
+import { FooterComponent } from '../../UI/shared/footer/footer.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [NavComponent],
+  imports: [NavComponent,FooterComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
